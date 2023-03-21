c96ccfadac19dae1651d1f27fcba1b0b12be7a76
RH-Xie <1748421646@qq.com>
2023-03-21 10:57:46 +0800

    feat: ref corner case

--- src/reactive/ref.js
+++ src/reactive/ref.js
@@ -1,4 +1,4 @@
-import { isObject } from "../../utils";
+import { hasChanged, isObject } from "../../utils";
 import { track, trigger } from "./effect";
 import { reactive } from "./reactive";
 
@@ -17,6 +17,8 @@ export function isRef(value) {
 class RefImpl {
     constructor(value) {
         this.__isRef = true;
+        // 可能为对象，然而用户要.value才获得reactive，要多写，所以应该是不推荐的
+        // 但作为开发者还是要转换
         this._value = convert(value);
     }
 
@@ -26,10 +28,14 @@ class RefImpl {
         return this._value;
     }
     set value(newValue) {
-        this._value = newValue;
-        // 触发依赖
-        // 注意是赋了新值再去触发
-        trigger(this, "value");
+        // 判断是否有变化，无变化则不trigger
+        if (hasChanged(this._value, newValue)) {
+            // 用户可能会将一个对象丢进来，要考虑，这时候value变为一个reactive
+            this._value = convert(newValue);
+            // 触发依赖
+            // 注意是赋了新值再去触发
+            trigger(this, "value");
+        }
     }
 }
 
